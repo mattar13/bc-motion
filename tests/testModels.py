6a01c84b0cfda982724db86cd6d5e693cb9ffa4d
mattar13 <mtarchick13@gmail.com>
2025-05-14 12:52:32 -0700

    Add initial test file for model functionality
    Created a new test file 'testModels.py' to facilitate testing of model components. The file includes basic imports and a placeholder for loading data, with a simple print statement for initial verification.

---
+++ tests/testModels.py
@@ -0,0 +1,10 @@
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# # import seaborn as sns #This is just for plotting, figure out later
+# from utils_modeling import *
+
+# BC_locations = np.load()
+
+if __name__ == "__main__":
+    print("Hello, World!")
